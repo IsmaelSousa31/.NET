db1d58c87f9d084dae62718ed05dcca66e8f885d
Ismael Sousa Silva <143268454+IsmaelSousa31@users.noreply.github.com>
2025-08-11 20:47:00 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Insira um numero inteiro: ");
+string inputnumeroint = Console.ReadLine();
+
+int numero_int = int.Parse(inputnumeroint);
+
+Console.Write("Insira um numero com casa decimais: ");
+string inputnumerofloat = Console.ReadLine();
+
+float numero_float = float.Parse(inputnumerofloat);
+
+Console.Write("Insira um caractere: ");
+string caractere = Console.ReadLine();
+
+char caractere_ = char.Parse(caractere);
+
+Console.Write("Insira um valor booleano(true ou false)");
+string valor_b = Console.ReadLine();
+
+bool valor_boleano = bool.Parse(valor_b);
+
+
+Console.WriteLine("Você inseriu: ");
+Console.WriteLine("Numero inteiro: " + numero_int);
+Console.WriteLine("Numero decimal: " +  numero_float);
+Console.WriteLine("Caractere: " + caractere_);
+Console.WriteLine("Valor Booleano: " + valor_boleano);
+
